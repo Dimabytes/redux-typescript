863244a1b7b002945e068c19bc5ced9b914ed200
Дмитрий Стовпец <dimabytes@gmail.com>
2020-08-30 13:24:26 +0300

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  useTabs: false,
+};
